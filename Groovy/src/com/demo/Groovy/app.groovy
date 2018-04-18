22b094ee499554fe6c69ae4e5f9d7c12b48a313e
wslyh123 <wslyh123@sohu.com>
2018-04-18 11:50:41 +0800

    groovy细节修改

--- Groovy/src/com/demo/Groovy/app.groovy
+++ Groovy/src/com/demo/Groovy/app.groovy
@@ -1,14 +1,15 @@
 package com.demo.Groovy
 
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ThisWillActuallyRun {
+class App {
 
     @RequestMapping("/")
-    String home() {
-        "Hello World!"
+    String home(@RequestParam String name) {
+        "Hello World!" + name
     }
 
 }
